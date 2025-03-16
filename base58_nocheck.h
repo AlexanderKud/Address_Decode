b4a32001dde3a5493c8aa2795acae0fc9c845007
8891689 <8891689@qq.com>
2025-03-16 14:56:05 +0800

    Update base58_nocheck.h

--- base58_nocheck.h
+++ base58_nocheck.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2025, wanghooj
+/* Copyright (c) 2025, 8891689
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
